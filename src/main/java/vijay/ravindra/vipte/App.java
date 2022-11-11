fae8d595aeb36c71dc6f53cd2100c80f41fbf895
vijayvipte <vijayvipte@gmail.com>
2022-11-11 19:24:27 +0530

    new

---
+++ src/main/java/vijay/ravindra/vipte/App.java
@@ -0,0 +1,13 @@
+package vijay.ravindra.vipte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
